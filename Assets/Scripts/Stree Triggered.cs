f9d485eb1e4b7ef5c82ae89b5251370b6e3b55d5
Beau Rubin-Rottenberg <beaurubrot@gmail.com>
2023-07-07 19:41:42 -0400

    death.

--- Assets/Scripts/Stree Triggered.cs
+++ Assets/Scripts/Stree Triggered.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class StreeTriggered : MonoBehaviour
 {
+    public float lifeLeft;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lifeLeft = 3;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -15,6 +17,11 @@ public class StreeTriggered : MonoBehaviour
         if (collision.gameObject.tag == "Drone")
         {
             Destroy(collision.gameObject);
+            lifeLeft--;
+            if (lifeLeft <= 0)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 
