20cded5eaca09ac4b5cd288ad6f734f78f0725eb
Beau Rubin-Rottenberg <beaurubrot@gmail.com>
2023-07-08 13:55:29 -0400

    Update DroneDeploy.cs

--- Assets/Scripts/DroneDeploy.cs
+++ Assets/Scripts/DroneDeploy.cs
@@ -21,6 +21,7 @@ public class DroneDeploy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         respawnTime = startRT;
         timeCounter = 0;
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
