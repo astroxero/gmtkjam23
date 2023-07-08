8f57cd6f14e13bd2d73594e321bfb40dbd58cffd
Beau Rubin-Rottenberg <beaurubrot@gmail.com>
2023-07-08 13:29:04 -0400

    running attempt

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public Animator anim;
     public Rigidbody2D rb;
     public float speed = 0;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     public void turnRight()
@@ -25,21 +27,25 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if ((Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.UpArrow)))
         {
             rb.AddForce(transform.up * speed);
+            anim.SetBool("isRunning", true);
         }
-        if (Input.GetKey(KeyCode.D))
+        if ((Input.GetKey(KeyCode.D)) || (Input.GetKey(KeyCode.RightArrow)))
         {
             rb.AddForce(transform.right * speed);
+            anim.SetBool("isRunning", true);
         }
-        if (Input.GetKey(KeyCode.S))
+        if ((Input.GetKey(KeyCode.S)) || (Input.GetKey(KeyCode.DownArrow)))
         {
             rb.AddForce(transform.up * -speed);
+            anim.SetBool("isRunning", true);
         }
-        if (Input.GetKey(KeyCode.A))
+        if ((Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.LeftArrow)))
         {
             rb.AddForce(transform.right * -speed);
+            anim.SetBool("isRunning", true);
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
@@ -48,6 +54,11 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D))
         {
             turnRight();
+        }
+
+        if ((!Input.GetKey(KeyCode.W)) && (!Input.GetKey(KeyCode.UpArrow)) && (!Input.GetKey(KeyCode.D)) && (!Input.GetKey(KeyCode.RightArrow)) && (!Input.GetKey(KeyCode.S)) && (!Input.GetKey(KeyCode.DownArrow)) && (!Input.GetKey(KeyCode.A)) || (!Input.GetKey(KeyCode.LeftArrow)))
+        {
+            anim.SetBool("isRunning", false);
         }
     }
 }
