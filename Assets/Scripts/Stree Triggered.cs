684321bbd1a490aab22285426be887e90f6e8b67
Beau Rubin-Rottenberg <beaurubrot@gmail.com>
2023-07-08 15:16:30 -0400

    IT WOOOOORKS!!!

--- Assets/Scripts/Stree Triggered.cs
+++ Assets/Scripts/Stree Triggered.cs
@@ -4,42 +4,80 @@ using UnityEngine;
 
 public class StreeTriggered : MonoBehaviour
 {
+    public delegate void CoroutineCallback();
+
+    public event CoroutineCallback OnWaitDroneComplete;
+    public event CoroutineCallback OnWaitTreeComplete;
+
     AudioSource audioSource;
     public float lifeLeft;
     public float totalLife = 3f;
-    public float droneDestroyDelay;
-    public float treeDestroyDelay;
+    public float droneWaitTime = 0.2f;
+    public float treeWaitTime = 3f;
+    private GameObject collidedDrone;
 
     // Start is called before the first frame update
     void Start()
     {
         lifeLeft = totalLife;
         audioSource = GetComponent<AudioSource>();
+
+        OnWaitDroneComplete += HandleWaitDroneComplete;
+        OnWaitTreeComplete += HandleWaitTreeComplete;
     }
+
+    private void HandleWaitDroneComplete()
+    {
+        if (collidedDrone != null)
+        {
+            Destroy(collidedDrone);
+            collidedDrone = null; // Reset the variable
+        }
+    }
+
+    private void HandleWaitTreeComplete()
+    {
+        Destroy(gameObject);
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Drone")
         {
-            Destroy(collision.gameObject);
             lifeLeft--;
-            audioSource.PlayOneShot(audioSource.clip,1.0F);
-
-            if (lifeLeft <= 0)
+            audioSource.PlayOneShot(audioSource.clip, 1.0F);
+            if (lifeLeft > 0)
             {
-
-                Destroy(gameObject);
+                StartCoroutine(waitDrone());
+                collidedDrone = collision.gameObject;
+            } else if (lifeLeft <= 0)
+            {
+                StartCoroutine(waitDrone());
+                collidedDrone = collision.gameObject;
+                StartCoroutine(waitTree());
             }
         }
     }
 
+    IEnumerator waitDrone()
+    {
+        yield return new WaitForSeconds(droneWaitTime);
+        OnWaitDroneComplete?.Invoke();
+    }
+    IEnumerator waitTree()
+    {
+        yield return new WaitForSeconds(treeWaitTime);
+        OnWaitTreeComplete?.Invoke();
+    }
+
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
         if (lifeLeft <= 0)
         {
             Destroy(gameObject);
         }
-    }
+    } */
 
 }
